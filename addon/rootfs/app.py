35360b43f89acc6c9206d31a5be11eb3fa0fb797
root <root@esp-programmer-dev.loc>
2026-07-04 09:18:25 +0000

    Add device info API

--- addon/rootfs/app.py
+++ addon/rootfs/app.py
@@ -9,6 +9,7 @@ from api.logs import router as logs_router
 from api.progress import router as progress_router
 from api.detect import router as detect_router
 from api.read_flash import router as read_flash_router
+from api.device_info import router as device_info_router
 from api.backup import router as backup_router
 
 app = FastAPI(title="MarKor ESP Programmer")
